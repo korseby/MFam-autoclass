67f8928490b5f1209b14fd2fd1822f89435ef993
Kristian Peters <kristian.peters@korseby.net>
2020-05-19 16:32:30 +0200

    fix htmlwidgets bug

--- galaxy/mfam-sunburst-interactive.r
+++ galaxy/mfam-sunburst-interactive.r
@@ -9,6 +9,7 @@ library(plotly)
 library(htmlwidgets)
 library(shiny)
 library(sunburstR)
+library(widgetframe)
 
 # Setup R error handling to go to stderr
 options(show.error.messages=F, error=function() { cat(geterrmessage(), file=stderr()); q("no",1,F) } )
